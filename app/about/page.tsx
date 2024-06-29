4d1d87083a62b30a9d6b5ff2ea08c0a03b4c37af
Daniel Bo <bodangren@gmail.com>
2024-06-29 11:30:27 +0800

    Added details to the About page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ export default function About() {
       <div>
         <h1>About My Blog</h1>
         <p>Welcome to my personal blog where I share my thoughts and experiences.</p>
+        <p>This is a project I created while using the LLM codecamp created by me. Haha.</p>
       </div>
     )
   }
