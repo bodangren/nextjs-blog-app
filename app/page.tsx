8ddc8b4d3f9ff64474695d8e0bf42224c559ac8f
Daniel Bo <bodangren@gmail.com>
2024-06-29 11:26:26 +0800

    Add About link to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link"
 export default function Home() {
   return (
     <main>
+      <Link href="/about">About</Link>
       <h1>Welcome to Dan's Blog</h1>
       <p>This is my first Next.js page!</p>
     </main>
