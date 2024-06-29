37b9de8178012c6c40c6cb19e67c9aadffa616d6
Daniel Bo <bodangren@gmail.com>
2024-06-29 11:11:22 +0800

    Add basic about page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About My Blog</h1>
+        <p>Welcome to my personal blog where I share my thoughts and experiences.</p>
+      </div>
+    )
+  }
